e0f059eedc7e7194a21a15dacaa6f38e3d5b7466
daigakusei2023 <121770821+daigakusei2023@users.noreply.github.com>
2023-02-11 00:55:31 +0100

    Add files via upload

---
+++ display.py
@@ -0,0 +1,91 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome import core, pins
+from esphome.components import display, spi
+from esphome.const import (
+    CONF_BUSY_PIN,
+    CONF_DC_PIN,
+    CONF_FULL_UPDATE_EVERY,
+    CONF_ID,
+    CONF_LAMBDA,
+    CONF_MODEL,
+    CONF_PAGES,
+    CONF_RESET_DURATION,
+    CONF_RESET_PIN,
+)
+
+DEPENDENCIES = ["spi"]
+
+hink_epaper_ns = cg.esphome_ns.namespace("hink_epaper")
+HinkEPaper = hink_epaper_ns.class_(
+    "HinkEPaper", cg.PollingComponent, spi.SPIDevice, display.DisplayBuffer
+)
+HinkEPaper1P54In = hink_epaper_ns.class_(
+    "HinkEPaper1P54In", HinkEPaper
+)
+HinkEPaper2P9In = hink_epaper_ns.class_(
+    "HinkEPaper2P9InB", HinkEPaper
+)
+HinkEPaper4P2In = hink_epaper_ns.class_(
+    "HinkEPaper4P2In", HinkEPaper
+)
+
+MODELS = {
+    "1.54in": (HinkEPaper1P54In),
+    "2.90in": (HinkEPaper2P9In),
+    "4.20in": (HinkEPaper4P2In),
+}
+
+def validate_full_update_every_only_types_ac(value):
+    return value
+
+
+CONFIG_SCHEMA = cv.All(
+    display.FULL_DISPLAY_SCHEMA.extend(
+        {
+            cv.GenerateID(): cv.declare_id(HinkEPaper),
+            cv.Required(CONF_DC_PIN): pins.gpio_output_pin_schema,
+            cv.Required(CONF_MODEL): cv.one_of(*MODELS, lower=True),
+            cv.Optional(CONF_RESET_PIN): pins.gpio_output_pin_schema,
+            cv.Optional(CONF_BUSY_PIN): pins.gpio_input_pin_schema,
+            cv.Optional(CONF_FULL_UPDATE_EVERY): cv.uint32_t,
+            cv.Optional(CONF_RESET_DURATION): cv.All(
+                cv.positive_time_period_milliseconds,
+                cv.Range(max=core.TimePeriod(milliseconds=500)),
+            ),
+        }
+    )
+    .extend(cv.polling_component_schema("1s"))
+    .extend(spi.spi_device_schema()),
+    validate_full_update_every_only_types_ac,
+    cv.has_at_most_one_key(CONF_PAGES, CONF_LAMBDA),
+)
+
+
+async def to_code(config):
+    model = MODELS[config[CONF_MODEL]]
+    rhs = model.new()
+    var = cg.Pvariable(config[CONF_ID], rhs, model)
+
+    await cg.register_component(var, config)
+    await display.register_display(var, config)
+    await spi.register_spi_device(var, config)
+
+    dc = await cg.gpio_pin_expression(config[CONF_DC_PIN])
+    cg.add(var.set_dc_pin(dc))
+
+    if CONF_LAMBDA in config:
+        lambda_ = await cg.process_lambda(
+            config[CONF_LAMBDA], [(display.DisplayBufferRef, "it")], return_type=cg.void
+        )
+        cg.add(var.set_writer(lambda_))
+    if CONF_RESET_PIN in config:
+        reset = await cg.gpio_pin_expression(config[CONF_RESET_PIN])
+        cg.add(var.set_reset_pin(reset))
+    if CONF_BUSY_PIN in config:
+        reset = await cg.gpio_pin_expression(config[CONF_BUSY_PIN])
+        cg.add(var.set_busy_pin(reset))
+    if CONF_FULL_UPDATE_EVERY in config:
+        cg.add(var.set_full_update_every(config[CONF_FULL_UPDATE_EVERY]))
+    if CONF_RESET_DURATION in config:
+        cg.add(var.set_reset_duration(config[CONF_RESET_DURATION]))
